84fcff2b2b5854fb84e08a9bfae9b12bd3007bfb
bradone-etole <omwitsabradone@gmail.com>
2022-04-18 14:52:37 +0300

    1.5

--- database.php
+++ database.php
@@ -1,9 +1,10 @@
 <?php
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'class');
+define('DB_SERVER', "us-cdbr-east-05.cleardb.net");
+define('DB_USERNAME', "bbee552e4676bf");
+define('DB_PASSWORD', "1e9d6152");
+define('DB_NAME', "heroku_f3ef71a21996103");
  
 
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
